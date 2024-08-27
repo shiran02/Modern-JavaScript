f2c060ebe29326ae21a7afd26707aaa121d174c6
Shiran Kumarasingha <shirankumarasingha97@gmail.com>
2024-08-27 15:11:59 +0530

    moduling and toolig

---
+++ 16-moduling-and-tooling/03-npm-install/app.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+
+
+async function getPost(){
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+    console.log(res.data);
+}
+
+
+getPost();
